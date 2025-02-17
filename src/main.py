d4f6e9ed1e3a10d211c48b9675d58c02a617ef43
ojlozagit <ojloza@gmail.com>
2025-02-17 02:07:13 -0500

    Preliminary work for static site generator, including introduction to html/css, and the creation of TextNode objects

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextType, TextNode
+
+def main():
+    node1 = TextNode("This is a text node",
+                      TextType.BOLD,
+                     "https://www.boot.dev")
+    print(node1)
+
+main()
