17c57da45edb9364faaae7733c656cad3e4a7e53
ojlozagit <ojloza@gmail.com>
2025-02-18 02:28:34 -0500

    refactored to only execute when file is directly executed

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from textnode import TextType, TextNode
 
+
 def main():
     node1 = TextNode("This is a text node",
                       TextType.BOLD,
                      "https://www.boot.dev")
     print(node1)
 
-main()
+
+if __name__ == "__main__":
+    main()
