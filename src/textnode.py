4a1581ec2139fcd10cdc931aa927ea11a2046770
ojlozagit <ojloza@gmail.com>
2025-02-23 16:09:41 -0500

    horizontal code cleanup of textnode.py

--- src/textnode.py
+++ src/textnode.py
@@ -43,4 +43,5 @@ def text_node_to_html_node(text_node):
             return LeafNode("img", "", {"src": text_node.url,
                                         "alt": text_node.text})
         case _:
-            raise ValueError(f"invalid text type: {text_node.text_type}")
+            raise ValueError( "invalid text type: "
+                             f"{text_node.text_type}")
