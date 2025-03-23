bd663e2b6150a0ba91fa58abee3fac425b3a84bd
ojlozagit <ojloza@gmail.com>
2025-03-23 08:47:36 -0400

    Expanded unordered lists to include list items that starts with '+' and '*'. Added markdown_to_html_node() function which generates the full html div, and its block/inline children, from a markdown document.

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,4 +1,9 @@
-from blocktype import *
+import re
+
+from textnode        import *
+from htmlnode        import ParentNode
+from blocktype       import BlockType
+from inline_markdown import text_to_textnodes
 '''
 @markdown: raw string (representing a full document)
 @return: list of "block" strings
@@ -33,9 +38,9 @@ def block_to_block_type(markdown):
             if not line.startswith(">"):
                 return BlockType.PARAGRAPH
         return BlockType.QUOTE
-    if markdown.startswith("- "):
+    if markdown.startswith(("- ", "* ", "+ ")):
         for line in lines:
-            if not line.startswith("- "):
+            if not line.startswith(("- ", "* ", "+ ")):
                 return BlockType.PARAGRAPH
         return BlockType.ULIST
     if markdown.startswith("1. "):
@@ -45,3 +50,138 @@ def block_to_block_type(markdown):
         return BlockType.OLIST
 
     return BlockType.PARAGRAPH
+
+'''
+@markdown: full markdown document
+@return: single parent HTMLNode
+'''
+def markdown_to_html_node(markdown):
+    block_nodes = []
+    md_blocks = markdown_to_blocks(markdown)
+    for block in md_blocks:
+        # Temporary conditional to help with VSCode intelisense
+        if not isinstance(block, str):
+            raise Exception("markdown_to_blocks failed")
+
+        block_type = block_to_block_type(block)
+        match block_type:
+            case BlockType.PARAGRAPH:
+                p_block = " ".join(block.split()).replace("\n", " ")
+                children = text_to_children(p_block)
+                block_nodes.append(ParentNode("p", children))
+
+            case BlockType.HEADING:
+            # - Markdown: Headings start with one or more #
+            #   characters followed by a space, e.g., # Heading 1 or
+            #   ### Heading 3.
+            # - HTML: These map to <h1> through <h6> tags based on
+            #   the number of # characters.
+            # - Key Rules:
+            #   - Trailing whitespace should be trimmed, as it is not
+            #     part of the heading.
+            #   - Headings generally do not preserve internal
+            #     excessive spaces (e.g., # A Heading becomes
+            #     <h1>A Heading).
+            #   - Heading content may include inline formatting such
+            #     as bold (**), italic (_), or links, which must also
+            #     be parsed.
+                h_count = block.find(" ")
+                h_block = block.removeprefix("#" * h_count + " " )
+                h_block = " ".join(h_block.split()).replace("\n", " ")
+
+                h_tag = f"h{h_count}"
+                children = text_to_children(h_block)
+                block_nodes.append(ParentNode(h_tag, children))
+
+            case BlockType.QUOTE:
+            # - Markdown: Lines prefixed with a > followed by a space
+            #   form a quote block. For example, > Quoted text.
+            # - HTML: Wrap content in a <blockquote> tag.
+            # - Key Rules:
+            #   - Multiple consecutive lines prefixed with > are
+            #     treated as part of the same quote block, where the
+            #     "> " is stripped, and newlines are replaced with a
+            #     single space.
+            #   - Single blank lines between > lines end one quote
+            #     block and start another.
+            #   - Inline formatting still applies inside the quoted
+            #     text (e.g., > **Bold quote** becomes
+            #     <blockquote><b>Bold quote</b></blockquote>).
+                block_lines = block.split("\n")
+                block_lines = list(map(lambda line: line[1:],
+                                       block_lines))
+                q_block = " ".join(" ".join(block_lines).split())
+
+                children = text_to_children(q_block)
+                block_nodes.append(ParentNode("blockquote", children))
+
+            case BlockType.OLIST:
+            # - Markdown: Lines starting with numbers followed by a
+            #   period and a space, e.g., 1. Item 1, 2. Item 2.
+            # - HTML: Map to an <ol> tag, with each list item inside
+            #   an <li>.
+            # - Key Rules:
+            #   - Line breaks/newlines within the list are ignored if
+            #     the lines are part of the same logical list.
+            #   - Items may contain inline formatting, which must be
+            #     parsed (e.g., 1. **Bold Item** becomes
+            #     <b>Bold Item</b> inside the <li>).
+            #   - Preserve the order of items, as 2. Item should
+            #     remain the second item.
+                lst_itms = block.split("\n")
+                ol_pattern = re.compile(r"\d+\. ")
+                lst_itms = list(map(lambda itm: re.sub(ol_pattern,
+                                                       "", itm, 1),
+                                    lst_itms))
+                lst_children = []
+                for text in lst_itms:
+                    children = text_to_children(text)
+                    lst_children.append(ParentNode("li", children))
+                block_nodes.append(ParentNode("ol", lst_children))
+
+            case BlockType.ULIST:
+            # - Markdown: Lines starting with -, *, or + followed by
+            #   a space, e.g., - Item 1, * Item 2.
+            # - HTML: Map to a <ul>, with each item wrapped in an
+            #   <li>.
+            # - Key Rules:
+            #   - Line breaks/newlines within the list are treated
+            #     similarly to ordered lists—ignored if logically
+            #     part of the same list.
+            #   - Like ordered lists, each list item may include
+            #     inline formatting (_italic_, **bold**, etc.) that
+            #     needs processing.
+            #   - Consistency of the bullet symbol (-, *, or +)
+            #     doesn't matter—they're all treated as the same in
+            #     markdown.
+                lst_itms = block.split("\n")
+                lst_itms = list(map(lambda itm: itm[2:], lst_itms))
+                lst_children = []
+                for text in lst_itms:
+                    children = text_to_children(text)
+                    lst_children.append(ParentNode("li", children))
+                block_nodes.append(ParentNode("ul", lst_children))
+
+            case BlockType.CODE:
+                code_block = block.removeprefix("```")
+                code_block = code_block.removeprefix("\n")
+                code_block = code_block.removesuffix("```")
+                code_text_node = TextNode(code_block, TextType.CODE)
+                child = text_node_to_html_node(code_text_node)
+                block_nodes.append(ParentNode("pre", [child]))
+            case _:
+                raise ValueError(f"Block Type: {block_type} not "
+                                 "handled by markdown_to_html_node()")
+    return ParentNode("div", block_nodes)
+
+"""
+@text: block of markdown text
+@return: list of html nodes corresponding to the inline text
+"""
+def text_to_children(text):
+    children = []
+    text_nodes = text_to_textnodes(text)
+    for node in text_nodes:
+        children.append(text_node_to_html_node(node))
+
+    return children
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -48,7 +48,7 @@ This is the same paragraph on a new line
         code_block  = "```if True:\n    hello world```"
         quot_block1 = ">no space quote"
         quot_block2 = "> space quote"
-        ul_block    = "- This is a list\n- with items"
+        ul_block    = "- This is a list\n* with several\n+ items"
         ol_block    = "1. This is an ordered list\n2. with items"
 
         # Error variables
@@ -96,3 +96,145 @@ This is the same paragraph on a new line
                          BlockType.PARAGRAPH)
         self.assertEqual(block_to_block_type(ol_err),
                          BlockType.PARAGRAPH)
+
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag"
+            " here</p><p>This is another paragraph with <i>italic"
+            "</i> text and <code>code</code> here</p></div>",
+    )
+
+    def test_headings(self):
+        self.maxDiff = None
+        md = """
+#      This is a
+Heading with   
+line
+breaks
+
+## This is    another
+Heading  with    long strings of spaces
+
+### This is a Heading with an **inline bold statement** 
+
+#### This is a Heading with _inline italics_
+
+##### This is a Heading with `inline code`
+
+###### This is a Heading with an [inline link](wowhead.com) and an
+![inline image](img.png)  
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            ("<div><h1>This is a Heading with line breaks</h1><h2>"
+             "This is another Heading with long strings of spaces"
+             "</h2><h3>This is a Heading with an <b>inline bold "
+             "statement</b></h3><h4>This is a Heading with <i>inline"
+             " italics</i></h4><h5>This is a Heading with <code>"
+             "inline code</code></h5><h6>This is a Heading with an "
+             "<a href=\"wowhead.com\">inline link</a> and an "
+             "<img src=\"img.png\" alt=\"inline image\"></img></h6>"
+             "</div>"),
+        )
+
+    def test_blockquote(self):
+        md = """
+>*First* line of the blockquote
+> __Second__ line of the blockquote
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            ("<div><blockquote><i>First</i> line of the blockquote "
+             "<b>Second</b> line of the blockquote</blockquote></div>"),
+        )
+
+    def test_ordered_list(self):
+        self.maxDiff = None
+        md = """
+1. _First_ entry in the list
+2. **Second** entry in the list
+3. `Third` entry in the list
+4. Fourth entry in the list
+5. Fifth entry in the list
+6. Sixth entry in the list
+7. Seventh entry in the list
+8. Eighth entry in the list
+9. Ninth entry in the list
+10. Tenth entry in the list
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            ("<div><ol><li><i>First</i> entry in the list</li><li>"
+             "<b>Second</b> entry in the list</li><li><code>Third"
+             "</code> entry in the list</li><li>Fourth entry in the "
+             "list</li><li>Fifth entry in the list</li><li>Sixth "
+             "entry in the list</li><li>Seventh entry in the list"
+             "</li><li>Eighth entry in the list</li><li>Ninth entry "
+             "in the list</li><li>Tenth entry in the list</li></ol>"
+             "</div>"),
+        )
+
+    def test_unordered_list(self):
+        self.maxDiff = None
+        md = """
+- *First* entry in the list
+* __Second__ entry in the list
++ `Third` entry in the list
++ Fourth entry in the list
+* Fifth entry in the list
+- Sixth entry in the list
+- Seventh entry in the list
+* Eighth entry in the list
++ Ninth entry in the list
+- Tenth entry in the list
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            ("<div><ul><li><i>First</i> entry in the list</li><li>"
+             "<b>Second</b> entry in the list</li><li><code>Third"
+             "</code> entry in the list</li><li>Fourth entry in the "
+             "list</li><li>Fifth entry in the list</li><li>Sixth "
+             "entry in the list</li><li>Seventh entry in the list"
+             "</li><li>Eighth entry in the list</li><li>Ninth entry "
+             "in the list</li><li>Tenth entry in the list</li></ul>"
+             "</div>"),
+        )
+
+    def test_codeblock(self):
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe"
+            " **same** even with inline stuff\n</code></pre></div>",
+    )
+
+if __name__ == "__main__":
+    unittest.main()
